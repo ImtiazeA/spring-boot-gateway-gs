f0ea5ead212e0ec7b12cb7c950b04b6161fc216c
Imtiaze A <imtiaze_a@yahoo.com>
2021-12-01 08:33:59 +0600

    Added Route to HTTPBin and Configured Circuit Breaker and Related Tests

---
+++ src/main/java/com/example/springbootgatewaygs/UriConfiguration.java
@@ -0,0 +1,12 @@
+//package com.example.springbootgatewaygs;
+//
+//import lombok.Data;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//
+//@ConfigurationProperties
+//@Data
+//public class UriConfiguration {
+//
+//    private String httpbin = "http://localhost:80";
+//
+//}
